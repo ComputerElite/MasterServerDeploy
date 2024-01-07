bdd92b2cccdd28c417781e3cf8f842926a515427
ComputerElite <71177995+ComputerElite@users.noreply.github.com>
2024-01-07 13:32:51 +0100

    use callback

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ const archiver = require('archiver');
 const github = require('@actions/github');
 
 try {
-  const token = core.getInput('token');
-  const serverName = core.getInput('server-name');
-  const directory = core.getInput('directory');
-  var serverUri = core.getInput('server-uri');
-  if(!serverUri.endsWith("/")) serverUri += "/";
-  serverUri += "api/updateserver?server=" + serverName + "&token=" + token;
+    const token = core.getInput('token');
+    const serverName = core.getInput('server-name');
+    const directory = core.getInput('directory');
+    var serverUri = core.getInput('server-uri');
+    if(!serverUri.endsWith("/")) serverUri += "/";
+    serverUri += "api/updateserver?server=" + serverName + "&token=" + token;
 
-  const file = `tmp.zip`
+    const file = `tmp.zip`
     // Create a writable stream to the zip file
     const output = fs.createWriteStream(file);
 
@@ -30,41 +30,45 @@ try {
     // Finalize the archive (write the zip file)
     archive.finalize();
 
+    output.on('end', () => {
+        console.log('Zip file created successfully. Posting update to server...')
+        const fileData = fs.readFileSync(file);
+        const options = {
+            method: 'POST',
+            headers: {
+            'Content-Type': 'application/octet-stream', // Set the content type to raw binary data
+            },
+        };
 
-  const fileData = fs.readFileSync(file);
-  const options = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/octet-stream', // Set the content type to raw binary data
-    },
-  };
+        const req = https.request(url, options, (res) => {
+            let responseData = '';
+        
+            // A chunk of data has been received.
+            res.on('data', (chunk) => {
+            responseData += chunk;
+            });
+        
+            // The whole response has been received.
+            res.on('end', () => {
+            console.log(responseData);
+            });
+        });
+        
+        // Handle errors
+        req.on('error', (error) => {
+            console.error(error);
+            core.setFailed(error);
+        });
+        
+        // Write the raw binary data to the request body
+        req.write(fileData);
+        
+        // End the request
+        req.end();
+    });
 
-  const req = https.request(url, options, (res) => {
-    let responseData = '';
-  
-    // A chunk of data has been received.
-    res.on('data', (chunk) => {
-      responseData += chunk;
-    });
-  
-    // The whole response has been received.
-    res.on('end', () => {
-      console.log(responseData);
-    });
-  });
-  
-  // Handle errors
-  req.on('error', (error) => {
-    console.error(error);
-    core.setFailed(error);
-  });
-  
-  // Write the raw binary data to the request body
-  req.write(fileData);
-  
-  // End the request
-  req.end();
+    
 
 } catch (error) {
-  core.setFailed(error.message);
+    core.setFailed(error.message);
 }
